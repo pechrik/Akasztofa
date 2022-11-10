17c35056cf19cb4a32ca64c50133c123bf2cb63b
Nyári Patrik 9B <nyari.patrik@students.jedlik.eu>
2022-11-10 21:31:24 +0100

    Játék vége, hibák javítása, finomítás

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ import random
 
 def menu():
     system('cls')
+    print(nehezseg)
     print(f'Menü\n\nKilépés\t\t\t(0)\nÚj játék\t\t(1)\nNehézség módosítása\t(2)\nSzavak módosítása\t(3)\n')
     valasz = input(f'Válasz: ')
     return valasz
@@ -23,19 +24,25 @@ def Kilepes():
 
 def UjJatek():
     system('cls')
-    win = ''
+    #konstansok
+    lose = False
+    elerhetobetuk = betuk
     hiba = 0
-    jelenlegiszo = random.randint(0,len(szavak)+1)
+    jelenlegiszo = random.randint(0,len(szavak)-1)
     kitalalt = []
-    allapot = False
     helyesvalasz = False
+
+    #játéloop
     while helyesvalasz != True:
+
+        #Játék elemek
         system('cls')
         print(f'Témakör: {temakorok[jelenlegiszo]}')
-        print(szavak[jelenlegiszo])
-        Szo(kitalalt, szavak[jelenlegiszo], win)
+        Szo(kitalalt, szavak[jelenlegiszo])
         Hangman(hiba)
         Betuk()
+
+        #Ellenőrzés
         valasztottbetu = input('Válassz egy betűt: ').lower()
         if valasztottbetu not in betuk:
             system('cls')
@@ -50,19 +57,45 @@ def UjJatek():
                 for i,betu in enumerate(szavak[jelenlegiszo]):
                     if betu == valasztottbetu:
                         kitalalt.append(i)
+                        for i,egybetu in enumerate(betuk):
+                            if valasztottbetu == egybetu:
+                                betuk.pop(i)
                 print(f'A(z) "{valasztottbetu}" betű megtalálható a szóban.')
 
+        #nyerés ellenőrzés
+        if len(kitalalt) == len(szavak[jelenlegiszo]):
+            system('cls')
+            print(f'A keresett szó: {szavak[jelenlegiszo].capitalize()}\nGratulálok, győztél\n\n+1 érme\n\n\n')
+            helyesvalasz = True
+            input('A továbblépéshez nyomja meg az ENTER gombot!')
 
-def Szo(kiirando, jelenlegiszo,kiszervezés):
+        #vesztés ellenőrzés
+        if nehezseg == 1:
+            if hiba > 9:
+                lose = True
+        if nehezseg == 2:
+            if hiba > 4:
+                lose = True
+        if nehezseg == 3:
+            if hiba > 2:
+                lose = True
+        if lose == True:
+            system('cls')
+            print(f'A keresett szó: {szavak[jelenlegiszo].capitalize()}\nVesztettél\n')
+            print('  _____\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n=========\n\n')
+            helyesvalasz = True
+            input('A továbblépéshez nyomja meg az ENTER gombot!')
+
+
+def Szo(kiirando, jelenlegiszo):
     listaszo = []
     for betu in jelenlegiszo:
-        listaszo.append(betu.lower())
+        listaszo.append(betu)
     for i, betu in enumerate(jelenlegiszo):
         if not i in kiirando:
             listaszo[i] = '_'
     vegleges = "".join(listaszo)
-    print(vegleges)
-    kiszervezés = vegleges
+    print(vegleges.capitalize())
 
 def Betuk():
     pass
@@ -110,37 +143,40 @@ def Hangman(hiba):
         if hiba == 3:
             print('  _____\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n=========\n')
 
-
-
 def Nehezseg():
-    valasz = False
-    while valasz != True:
+    loop = False
+    while loop != True:
         system('cls')
-        print(f'A játék nehézségének módosítása\n\nKönnyű\t10 próbálkozás \t(1)\nKözepes\t5 próbálkozás\t(2)\nNehéz\t3 próbálkozás\t(3)\n')
-        nehezseg = input(f'Válasz:')
-        if nehezseg == '1':
+        if nehezseg == 1:
+            nehezszoval = 'Könnyű'
+        if nehezseg == 2:
+            nehezszoval = 'Közepes'
+        if nehezseg == 3:
+            nehezszoval = 'Nehéz'
+        print(f'A játék nehézségének módosítása\n\nKönnyű\t10 próbálkozás \t(1)\nKözepes\t5 próbálkozás\t(2)\nNehéz\t3 próbálkozás\t(3)\nJelenlegi nehézség:{nehezszoval}\n')
+        valasz = input(f'Válasz:')
+        if valasz == '1':
             system('cls')
-            valasz = True
+            loop = True
             print('Nehézség beállítva a következőre: Könnyű')
             time.sleep(1.5)
-            return nehezseg
-        elif nehezseg == '2':
+            return int(valasz)
+        elif valasz == '2':
             system('cls')
-            valasz = True
+            loop = True
             print('Nehézség beállítva a következőre: Közepes')
             time.sleep(1.5)
-            return nehezseg
-        elif nehezseg == '3':
+            return int(valasz)
+        elif valasz == '3':
             system('cls')
-            valasz = True
+            loop = True
             print('Nehézség beállítva a következőre: Nehéz')
             time.sleep(1.5)
-            return nehezseg
+            return int(valasz)
         else:
             system('cls')
             print('Hibás válasz')
             time.sleep(1.5)
-            valasz = False
 
 def Szavak():
     pass
